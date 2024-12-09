d72ec204036c4ca3891a27176319e8b5936c3436
anisalil10 <aniketh.salil@mail.utoronto.ca>
2024-12-09 03:53:35 -0500

    Delete src/Main/use_cases/open_recipe directory

--- src/Main/use_cases/open_recipe/OpenRecipeInputBoundary.java
+++
@@ -1,8 +0,0 @@
-package use_cases.open_recipe;
-
-public interface OpenRecipeInputBoundary {
-
-    void execute(OpenRecipeInputData openRecipeInputData);
-
-    void addToFavourites(OpenRecipeInputData openRecipeInputData);
-}
